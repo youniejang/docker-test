4c24ae91387d0456842e1bd59955484846adbc6e
younie.jang@gmail.com <younie.jang@gmail.com>
2022-10-01 19:30:19 +0900

    git build test

---
+++ src/main/java/aws/test/demo/controller/SampleController.java
@@ -0,0 +1,14 @@
+package aws.test.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/sample")
+    public String sample() {
+        return "sample";
+    }
+
+}
